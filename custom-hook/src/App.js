d45119a610c37c795b51491dcb897aa8e05234ea
Jessica Latorre Cabral <jessica.latorre182@gmail.com>
2021-08-31 16:36:48 -0300

    correção de erro no código do hook

--- custom-hook/src/App.js
+++ custom-hook/src/App.js
@@ -1,37 +1,27 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import useApiCall from './hook/useApiCall';
 
 const App = () => {
 
   const[pokemonName, setPokemonName] = useState("") 
   const {data, isLoading, getData} = useApiCall(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-  // console.log(data,response,errors)
 
   const changePokemonName = (e) =>{
     setPokemonName(e.target.value)
-    
   }
-  
-  
-  // useEffect(()=>{
-  //   setIsLoading(false)
-  // },[pokemonName])
 
   return (
     <div>
       <label>Digite o nome de algum pokémon:</label>
       <input placeholder={"ex.: eevee"} value={pokemonName} onChange={changePokemonName}/>
       <button onClick={getData}>buscar pokemon</button>
-      {isLoading && <p>Carregando...</p>}  
-      <>
+
+      {isLoading ? <p>Carregando...</p> :
+      (<>
         <p>Nome: {data.name}</p>
-         <p>Peso: {data.weight} kg</p> 
-      </>
-          
-         
-        
-      
-        
+        <p>Peso: {data.weight} kg</p> 
+      </>)}
+
     </div>
   )
 }
--- custom-hook/src/hook/useApiCall.js
+++ custom-hook/src/hook/useApiCall.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import {useState} from "react"
 import axios from 'axios'
 
 const useApiCall = (url) => {
@@ -8,60 +8,24 @@ const useApiCall = (url) => {
   const[response, setResponse] = useState()
   const[data, setData]= useState({})
 
-  const getData =  () =>
-          // setIsLoading(true)
-    
-        axios
-          .get(url)
+  const getData =  () => {
+      setIsLoading(true)
+      axios
+      .get(url)
+      .then((response) => {
+        setIsLoading(false)
+        setResponse(response)
+        setData(response.data)
+      })
+      .catch((err) => {
+        setErrors(err)
+        setIsLoading(false)
+        alert('Ocorreu um erro, tente novamente')
+      })
+  }
 
-          .then((response) => {
-
-            setResponse(response)
-            setData(response.data)
-            setIsLoading(false)
-
-
-          })
-          .catch((err) => {
-            setErrors(err)
-            // alert('ocorreu um erro, tente novamente')
-            setIsLoading(false)
-          }
-    
-  )
-
-  // useEffect(() => {
-  //   getData();
-  // }, [data]);
-
-  return {data, getData, isLoading, response, errors}
-
- 
+  return {data, getData, isLoading}
   
-
-    // const[isLoading, setIsLoading] = useState(true)
-    // const[errors, setErrors] = useState()
-    // const[response, setResponse] = useState()
-    // const[data, setData]= useState({})
-
-    // const getData =  () => {
-    //     axios
-    //       .get(url)
-    //       .then((response) => {
-    //           setResponse(response)
-    //         setData(response.data)
-    //         setIsLoading(false);
-    //       })
-    //       .catch((err) => {
-    //         setErrors(err)
-    //         setIsLoading(false);
-    //       });
-    //   };
-
-     
-
-    //   return {data, isLoading, getData, response, errors}
-    
 }
 export default useApiCall
 
